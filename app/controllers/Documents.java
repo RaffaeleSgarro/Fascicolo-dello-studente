ff69e0e483cd6d8dc272b670cbfc3103de2829f0
Raffaele Sgarro <raffaeleSgarro@gmail.com>
2012-02-06 23:01:52 +0100

    Added Documents controller

---
+++ app/controllers/Documents.java
@@ -0,0 +1,16 @@
+package controllers;
+
+import play.mvc.Controller;
+
+public class Documents extends Controller {
+
+	public static void index() {
+		render();
+	}
+	
+	public static void create() {}
+	
+	public static void edit() {}
+	
+	public static void delete() {}
+}
