190cbad344900a7a3f7cffc07cd7db55fa4ef76d
Raffaele Sgarro <raffaeleSgarro@gmail.com>
2012-02-06 19:38:54 +0100

    Basic test added

--- test/BasicTest.java
+++ test/BasicTest.java
@@ -1,4 +1,7 @@
 import org.junit.*;
+
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.*;
 import play.test.*;
 import models.*;
@@ -6,8 +9,18 @@ import models.*;
 public class BasicTest extends UnitTest {
 
     @Test
-    public void aVeryImportantThingToTest() {
-        assertEquals(2, 1 + 1);
+    public void testDocument() throws MalformedURLException {
+    	Document doc = new Document();
+    	String google = "http://www.google.com";
+    	doc.url = new URL(google);
+    	doc.save();
+    	
+    	doc = Document.find("byUrl", new URL(google)).first();
+    	//
+    	assertNotNull(doc);
+    	assertEquals(doc.url, new URL(google));
+    	
+    	doc.delete();
     }
 
 }
