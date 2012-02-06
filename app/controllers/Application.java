c971701f1db29f2ad3faf0b0e98ea77345b24ec2
Raffaele Sgarro <raffaeleSgarro@gmail.com>
2012-02-06 23:05:00 +0100

    Application controller index() redirects to Documents.index()

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,16 +1,11 @@
 package controllers;
 
-import play.*;
-import play.mvc.*;
-
-import java.util.*;
-
-import models.*;
+import play.mvc.Controller;
 
 public class Application extends Controller {
 
     public static void index() {
-        render();
+        redirect("Documents.index");
     }
 
 }
