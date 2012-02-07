5b751d6d78e69747d0b62e03bd8ff256be289199
Raffaele Sgarro <raffaeleSgarro@gmail.com>
2012-02-08 00:20:42 +0100

    Documents form() and save() implementations + general cleanup

--- app/controllers/Documents.java
+++ app/controllers/Documents.java
@@ -1,6 +1,7 @@
 package controllers;
 
 import models.Document;
+import play.data.validation.Valid;
 import play.mvc.Controller;
 
 import java.net.MalformedURLException;
@@ -10,29 +11,27 @@ import java.util.List;
 public class Documents extends Controller {
 
 	public static void index() {
-		List<Document> documents = Document.findAll();
+		List<Document> documents = Document.all().fetch(1000);
 		render(documents);
 	}
 	
-	public static void create(String url, String title, String student_pin) {
-		if (url == null || title == null || student_pin == null) {
-			render();
-		} else {
-			Document doc = new Document();
-			doc.student_pin = student_pin;
-			try {
-				doc.url = new URL(url);
-			} catch (MalformedURLException e) {
-				render();
-				return;
-			}
-			doc.title = title;
-			doc.save();
-			redirect("Documents.index");
-		}
+	public static void findByStudentPin(String student_pin) {
+		List<Document> documents = Document.findByStudentPin(student_pin);
+		render("@index", documents);
 	}
 	
-	public static void edit() {}
+	public static void save(@Valid Document document) {
+		if (validation.hasErrors()) {
+			render("@form", document);
+		}
+		document.save();
+		redirect("Documents.index");
+	}
+	
+	public static void form(Long id) {
+		Document document = Document.findById(id);
+		render("@form", document);
+	}
 	
 	public static void delete(Long id) {
 		Document doc = Document.findById(id);
--- app/models/Document.java
+++ app/models/Document.java
@@ -1,17 +1,28 @@
 package models;
 
 import java.net.URL;
+import java.util.LinkedList;
 import java.util.List;
 
 import javax.persistence.Entity;
 
+import play.data.validation.Required;
 import play.db.jpa.Model;
 
 @Entity
 public class Document extends Model {
 
+	@Required
 	public String title;
-	public URL url;
-	public String student_pin;
 	
+	@Required
+	public String url;
+	
+	@Required
+	public String studentPin;
+	
+	public static List<Document> findByStudentPin(String student_pin) {
+		return Document.find("byStudentPin", student_pin).fetch();
+	}
+
 }
